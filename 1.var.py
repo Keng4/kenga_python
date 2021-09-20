62790ee1b846816439198542c84aee9e9eac856e
keng4 <muhoplecius@mail.ru>
2021-09-20 20:59:36 +0900

    create 1.var.py

---
+++ 1.var.py
@@ -0,0 +1,10 @@
+# *** ПЕРЕМЕННЫЕ ***
+
+# ctrl+"/" - вызов решетки
+# объявление переменной
+my_var_1 = 100 #snace-case
+myVar2 = 200 # camel-case
+z = my_var_1 + myVar2
+#встроенная функция: 
+print(z)
+# ctrl + J
